079a2a6d074bc7649b9efc8074c5731cbc313274
AJ Helfen <ahelfen@SCLTMAC-10081.local>
2019-01-14 10:24:27 -0500

    module 2.1 and 2.2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,5 +1,10 @@
 <template>
     <h1>{{ title }}</h1>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
 </template>
 
 <script>
@@ -7,12 +12,20 @@
         name: 'BookList',
         data: {
             title: 'All Books'
-        }
+        },
     }
 </script>
 
 <style scoped>
     h1, h2 {
         font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
     }
 </style>
